ac805811dd622e8c4f4145d352f898953b803e2b
p4v4n <pavan.barcelona@gmail.com>
2017-08-24 16:23:18 +0530

    Add more examples from c-3

--- chapter-3/examples.clj
+++ chapter-3/examples.clj
@@ -40,4 +40,38 @@
        list2)))
 ;;Complexity: O(n) Proportional to the length of the list being copied
 
+;;Appending an item to a list
 
+(defn append-item [some-list item]
+  (let [[x & xs] some-list]
+    (if x
+        (conj (append-item xs item) x)
+        (list item))))
+;;Complexity: O(n) costly for longer lists.Better to use vectors in such cases.
+
+;;Prepending an item to a list
+
+(defn prepend-item [some-list item]
+  (conj some-list item))
+;;Complexity: O(1) 
+
+;;Getting value at index
+
+(defn elemAtIndex [some-list ind]
+  (let [[x & xs] some-list]
+    (if (zero? ind)
+        x
+        (elemAtIndex xs (dec ind)))))
+;;assumes ind >= 0
+;;Complexity: O(1)
+
+;;Modifying a list value
+
+(defn set-element [some-list ind elem]
+  (if (empty? some-list)
+      "Index not valid"
+      (let [[x & xs] some-list]
+        (if (zero? ind)
+            (conj xs elem)
+            (conj (set-element xs (dec ind) elem) x)))))
+;;to change: fails for invalid indices
